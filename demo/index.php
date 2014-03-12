9854faee7c74adeb4f92cbc741bc056c1616368b
blink <blinkcao@gmail.com>
2014-03-12 15:01:23 +0800

    delete

--- demo/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-use EPS\Process\MainProcess;
-define('SERVER_PATH', __DIR__ . '/');
-define('LOG_FILE', __DIR__ . '/ttt.log');
-$loader = include SERVER_PATH . 'autoload.php';
-MainProcess::instance('eps_main')
-    ->setWorker('Server\\Worker\\Main')
-    ->setMain()
-    ->run();
